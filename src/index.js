eab05cf4273810979f10bef22e1680c12a12ce03
matias <matias3282@hotmail.com>
2022-09-07 17:55:24 -0300

    class game, class enemies

--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
 /**@type {HTMLCanvasElement}*/
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 800;
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 800;
 
-class Game {
-  constructor() {
+  class Game {
+    constructor(ctx, width, height) {
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
+      this.#addNewEnemy();
+      console.log(this.enemies);
+    }
+    update() {
+      this.enemies.forEach(object => object.update())
+    }
+    draw() {
+      this,this.enemies.forEach(object => object.draw())
+    }
+    #addNewEnemy() {
+      this.enemies.push(new Enemy())
+    }
   }
-  update() {}
-  draw() {}
-  #addNewEnemy(){}
-}
 
-class Enemy{
-    constructor(){
+  class Enemy {
+    constructor() {
+      this.x = 100;
+      this.y = 100;
+      this.width = 100;
+      this.width = 100;
+    }
+    update() {
+      this.x --;
+    }
+    draw() {
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
 
-    }
-    update(){
+  const game = new Game(ctx, canvas.width, canvas.height)
+  let lastTime = 1;
 
-    }
-    draw(){
-        
-    }
-}
+  function animate(timeStamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    game.update();
+    game.draw();
+    //some code
+    requestAnimationFrame(animate);
+  };
+  animate(0);
+});
